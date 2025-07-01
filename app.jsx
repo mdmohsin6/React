c591a53138f059c8f053daf72e3b2ab0c2eadb6c
Md Mohsin <108623805+mdmohsin6@users.noreply.github.com>
2025-07-01 12:30:20 +0530

    Update app.jsx

--- app.jsx
+++ app.jsx
@@ -1,1 +1,44 @@
-app.jsx
+import { useState } from "react";
+
+function Skills(){
+    const [skills, setSkills] = useState([])
+    const handleSkills = (event) =>{
+        console.log(event.target.value, event.target.checked);
+        if(event.target.checked){
+            setSkills([...skills,event.target.value])
+
+        }else{
+            setSkills([...skills.filter((item)=>item!=event.target.value)]);
+        }
+    }
+
+    
+    return (
+        <div>
+           <h3>Select your skills</h3>
+           <input onChange={handleSkills} type="checkbox" id="php" value="php" />
+           <label htmlFor="php">PHP</label>
+           <br />
+
+           <input onChange={handleSkills} type="checkbox" id="node" value="node" />
+           <label htmlFor="node">node</label>
+           <h1>{skills.toString()}</h1>
+
+        </div>
+    )
+}
+export default Skills;
+
+Skills fil;e
+
+import Skills from "./Skills";
+
+function App(){
+  return (
+    <div>
+      <h1>Learning checkbox in react js</h1>
+      <Skills />
+    </div>
+  )
+}
+export default App;
